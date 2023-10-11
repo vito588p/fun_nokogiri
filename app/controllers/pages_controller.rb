b186850c233fb4c0eeceb9b92e9fa76d03d8a150
Weber <vito588p@gmail.com>
2023-10-11 15:16:35 +0800

    feat: add wicked_pdf in controller and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,22 @@ class PagesController < ApplicationController
   def index
     url = "https://www.tenlong.com.tw/zh_tw/recent_bestselling?range=30"
     @doc = Nokogiri::HTML(URI.open(url))
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "file_name", template: "pages/index", formats: [:html], encoding: 'UTF-8' , dpi: 72
+      end
+    end
   end
 
   def show
     url = "https://mops.twse.com.tw/mops/web/t05sr01_1"
     @doc = Nokogiri::HTML(URI.open(url))
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "file_name", template: "pages/show", formats: [:html], encoding: 'UTF-8' , dpi: 72
+      end
+    end
   end
 end
